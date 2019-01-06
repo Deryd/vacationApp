3e54f27097b8783fcb80d6d178bea0dad85e4ea6
Volodymyr Deryd <deridv@gmail.com>
2019-01-06 08:14:38 +0200

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VacationApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
